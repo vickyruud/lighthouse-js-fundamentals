d16b0232f2e52f62d1945ea73902eb5e6885d8ef
Aprajit Kumar <aprajit.k@gmail.com>
2021-06-28 21:33:34 +0000

    completed looplighthouse assignment

---
+++ loopylighthouse.js
@@ -0,0 +1,17 @@
+let i = 100;
+for (let i = 100; i <= 200; i++){
+  if(i % 3 === 0 && i % 4 !== 0){
+    console.log("Loopy");
+  }
+  else if(i % 4 === 0 && i % 3 !== 0){
+    console.log("Lighthouse");
+
+  }
+  else if (i % 3 === 0 && i % 4 === 0){
+    console.log("LoopyLighthouse");
+  }
+  else{
+    console.log(i);
+  }
+  
+}
