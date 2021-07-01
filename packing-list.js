12406d48a310d167ad9142b78e33790873bf1d41
Aprajit Kumar <aprajit.k@gmail.com>
2021-07-01 14:55:52 +0000

    packing list

--- packing-list.js
+++ packing-list.js
@@ -1,6 +1,7 @@
 const packingList = ["bowls", "plates", "pots", "pans", "eating utensils", "glasses", "cups", "cooking utensils"];
-
+let i = 0;
 console.log("Kitchen stuff to pack:");
-for (let i = 0; i < packingList.length; i++) {
+while( i < packingList.length){
   console.log(packingList[i]);
-}
+  i++;
+  }
